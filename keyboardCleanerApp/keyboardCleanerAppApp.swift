b6202376e6431e1cabcf191afae3a516a76a3c94
Martin G <jmartin.galindob@gmail.com>
2025-10-30 19:13:32 -0500

    Initial Commit

---
+++ keyboardCleanerApp/keyboardCleanerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  keyboardCleanerAppApp.swift
+//  keyboardCleanerApp
+//
+//  Created by Martin Galindo on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct keyboardCleanerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ keyboardCleanerAppTests/keyboardCleanerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  keyboardCleanerAppTests.swift
+//  keyboardCleanerAppTests
+//
+//  Created by Martin Galindo on 10/30/25.
+//
+
+import Testing
+@testable import keyboardCleanerApp
+
+struct keyboardCleanerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
